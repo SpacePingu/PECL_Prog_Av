e3c63ee77ece9534cc5e473bebca2ef180e81e4b
Adryh <adryhm4@gmail.com>
2021-04-30 17:13:13 +0200

    Update PracticaAvanzada.java

--- PracticaAvanzada/src/practicaavanzada/PracticaAvanzada.java
+++ PracticaAvanzada/src/practicaavanzada/PracticaAvanzada.java
@@ -16,7 +16,7 @@ public class PracticaAvanzada {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HELLOOOOOO");
+        System.out.println("NO");
     }
     
 }
